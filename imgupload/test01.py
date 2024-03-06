b0d2e978db5d9e595a49a9625b4816a30eaeca8f
Scarlet <bhandwalkartanish513@gmail.com>
2024-03-06 09:10:45 +0530

    plant img upload working.

--- imgupload/test01.py
+++ imgupload/test01.py
@@ -8,17 +8,19 @@ def encode_file(file_name):
 
 
 def identify_plant(file_names):
+
     # see the docs for more optional attributes
+    
+        # disease details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Disease-details
+        # plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
     params = {
         "api_key": "r9T54HmSyf6xc1TBgS6HH1FThIa8Z3IhYoU2MalueQ0IPZT6f4",
         "images": [encode_file(img) for img in file_names],
         "latitude": 49.1951239,
         "longitude": 16.6077111,
         "datetime": 1582830233,
-        # modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
         "modifiers": ["crops_fast", "similar_images", "health_all", "disease_similar_images"],
         "plant_language": "en",
-        # plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
         "plant_details": ["common_names",
                           "edible_parts",
                           "gbif_id"
@@ -30,18 +32,15 @@ def identify_plant(file_names):
                           "wiki_description",
                           "wiki_image",
                           ],
-        # disease details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Disease-details
         "disease_details": ["common_names", "url", "description"]
         }
 
     headers = {
         "Content-Type": "application/json"
         }
-
     response = requests.post("https://api.plant.id/v2/identify",
                              json=params,
                              headers=headers)
-
     return response.json()
 
 
